4b9df2736d7567b27d820375dca210a73a2dfac2
kyad-lang-lux <emrissoglo@gmail.com>
2026-03-25 11:27:58 +0100

    Mises à jour sur la branche emerus

--- app/(dashboard)/admin/page.js
+++ app/(dashboard)/admin/page.js
@@ -16,7 +16,7 @@ export default function AdminLoginPage() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    if (auth.user === "admin" && auth.pass === "1234") {
+    if (auth.user === "admin" && auth.pass === "admin@ulrich") {
       localStorage.setItem("admin_token", "true");
       router.push("/admin/notifications");
     } else {
